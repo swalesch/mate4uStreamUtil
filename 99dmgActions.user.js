b9c009216ac163952a1f7fe7914330b11b181b5b
Stefan Walesch <wallinator@gmx.de>
2019-11-01 16:30:31 +0100

    Update 99dmgActions.user.js
    - if no vote exists the page will turn green and wait
    - add intervall to search for vote

--- 99dmgActions.user.js
+++ 99dmgActions.user.js
@@ -29,9 +29,8 @@
     function toggleActive(caller){
         Array.prototype.slice.call(caller.path[3].getElementsByTagName("li")).forEach(
             function(item){
-            item.classList.remove('active');
+                item.classList.remove('active');
             })
-
         caller.path[2].classList.add('active');
     }
 
@@ -47,15 +46,26 @@
     }
 
     function hideAll(){
-        var mapvote = document.getElementById('mapvote');
-
-        document.body.append(mapvote);
         document.body.childNodes.forEach(function(item){
             if(item.style != undefined) {
                 item.style.display = 'none'};
         });
+        document.body.style.background = "#00ff00 no-repeat right top";
 
-        document.body.lastChild.style.display = 'block';
-        document.body.style.background = "#008000 no-repeat right top";
+        if(document.getElementById('mapvote')==null){
+            var interval = setInterval(() => {
+                console.log("look for vote");
+                if(document.getElementById('mapvote')!=null){
+                    var mapvote = document.getElementById('mapvote');
+                    document.body.append(mapvote);
+                    document.body.lastChild.style.display = 'block';
+                    clearInterval(interval);
+                }
+            }, 3000);
+        }else{
+            var mapvote = document.getElementById('mapvote');
+            document.body.append(mapvote);
+            document.body.lastChild.style.display = 'block';
+        }
     }
 })();
