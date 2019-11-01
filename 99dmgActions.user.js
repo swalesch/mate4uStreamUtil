7fad071738046ae08dbda17d6222eadedab779b0
Stefan Walesch <wallinator@gmx.de>
2019-11-01 17:59:28 +0100

    Update 99dmgActions.user.js
    change images

--- 99dmgActions.user.js
+++ 99dmgActions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         99dmgActions
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  Show only the Map Vote on a 99dmg match site.
 // @author       Hive
 // @match        https://csgo.99damage.de/de/leagues/matches/*
@@ -81,6 +81,7 @@
                 if(item.innerHTML!=undefined){
                     var mapItem = findObjectByAttribute(maps,'name',item.innerHTML);
                     item.style.backgroundImage = "url("+mapItem.image+")";
+                    item.style.backgroundSize = mapItem.size;
                     item.style.width = "200px";
                     item.style.height = "200px";
                     item.style.border = "none";
@@ -101,30 +102,40 @@
     var maps =
         [{
             name: 'de_dust2',
-            image: 'https://lh6.googleusercontent.com/qBzEuMdPHw-8fapEd97NNjEH4osfZEYf27R8djPxQiCsgLP8OYtNsThJ_XyyqeyKyEuTQyD3mvsFn6jRjvox=w1920-h969'},
+            image: 'https://www.csgodatabase.com/images/pins/Dust_II_Pin.png',
+            size:"200px 200px"},
          {
              name: 'de_inferno',
-             image: 'https://lh3.googleusercontent.com/tYprhlwObnTyr38VpYkdX3-cSdQKbEYbLjz_12sTVtY8M0P9zQietDQ4h8sTqhGDx38jmuNIkpYzkPL83Vk5=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/pins/Inferno_Pin.png',
+             size:"200px 200px"},
+
          {
              name: 'de_mirage',
-             image: 'https://lh3.googleusercontent.com/-XX60dGrcPkPzkSuZBlC-xp-t9Wii4WKU8usjhujJAdz05-7hblDNQ-xnvxLEqDKJA7TYG7IvYq-p8D_ZRMC=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/pins/Mirage_Pin.png',
+             size:"200px 200px"},
          {
              name: 'de_nuke',
-             image: 'https://lh4.googleusercontent.com/WtpWTBUDsKfzoM_Q-jnHjpk716_9FjPuulJxyMgZ-R4alAqePhANdGdVvAx0bLaZoM2ENLA_SqGdAwlP1j8L=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/pins/Nuke_Pin.png',
+             size:"200px 200px"},
          {
              name: 'de_overpass',
-             image: 'https://lh4.googleusercontent.com/doUu0Lf7V4KnKVNPejAOBN9m3X0G2rFTJepjkancNmRPNS7wrkGME8hI0QyLJStTZnKcDhZT1cWHeV0Xuav2=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/pins/Overpass_Pin.png',
+             size:"200px 200px"},
          {
              name: 'de_train',
-             image: 'https://lh3.googleusercontent.com/YJE-UbGgvL_ivxeTczBRpY-QlTozsS9dwWLNFL3LjHJl0XL7A-aSknJtcESdWYcezP5ugfwORo_R-Kv2ge7d=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/pins/Train_Pin.png',
+             size:"200px 200px"},
          {
              name: 'de_vertigo',
-             image: 'https://lh4.googleusercontent.com/c81bvQnTiHKEjk8IV_hlB84WlzhYfXMDFxEv-RcJwKYsCtO5YPjWZgq7MOBx_wgDk1-ZABM4t7el6fFQylgK=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/collections/Vertigo.png',
+             size:"200px 182px"},
          {
              name: 'de_cache',
-             image: 'https://lh4.googleusercontent.com/3EY3ai3NjHh2TAYiUBirZ78U_JFqTE3gQpJXo2yt-hT7y9jmAIW-MkfgfkHIOc560iUPsw4PGOyC5kChUbdv=w1920-h969'},
+             image: 'https://www.csgodatabase.com/images/pins/Cache_Pin.png',
+             size:"200px 200px"},
          {
              name: 'de_cbble',
-             image: 'https://lh5.googleusercontent.com/3cTybs0deKxc9-ITwIl38hslDoTQ82NVJgxP_Duij_Ik7FuKBpAlD0oCHBllc-bfZeNYAjAIbQPy10R75br3=w1920-h969'}];
+             image: 'https://www.csgodatabase.com/images/pins/Cobblestone_Pin.png',
+             size:"200px 200px"}];
 
 })();
