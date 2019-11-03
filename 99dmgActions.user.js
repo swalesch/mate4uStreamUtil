45be111c644fd328a034a89a66d2c2d29cbbf9e0
Stefan Walesch <wallinator@gmx.de>
2019-11-03 18:07:05 +0100

    Update 99dmgActions.user.js

--- 99dmgActions.user.js
+++ 99dmgActions.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         99dmgActions
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Show only the Map Vote on a 99dmg match site.
 // @author       Hive
 // @match        https://csgo.99damage.de/de/leagues/matches/*
@@ -11,28 +11,28 @@
 (function() {
     'use strict';
     GM_addStyle ( `
-.line1 {
+.cross1 {
 width: 260px;
 height: 100px;
 border-bottom: 4px solid red;
 -webkit-transform: translateX(10px) translateY(10px) rotate(45deg);
 position: absolute;
 }
-.line2 {
+.cross2 {
 width: 260px;
 height: 100px;
 border-bottom: 4px solid red;
 -webkit-transform: translateX(-70px) translateY(10px)rotate(-45deg);
 position: absolute;
 }
-.strich1 {
+.checkmark1 {
 width: 40px;
 height: 101px;
 border-bottom: 6px solid blue;
 -webkit-transform: translateX(94px) translateY(26px)rotate(45deg);
 position: absolute;
 }
-.strich2 {
+.checkmark2 {
 width: 80px;
 height: 101px;
 border-bottom: 6px solid blue;
@@ -77,27 +77,38 @@ position: absolute;
     function activateSkript(){
         document.body.childNodes.forEach((item) => {
             if(item.style != undefined) {
-                item.style.display = 'none'};
+                item.style.display = 'none';
+            }
         });
         document.body.style.background = "#00ff00 no-repeat right top";
-
+        var doOnce=0;
         if(document.getElementById('mapvote')==null || document.getElementsByClassName('mapvote-maps-done').length == 0){
             var intervalChangeHeader = setInterval(() => {
                 console.log("look for header");
-                if(document.getElementById('mapvote')!=null && document.getElementById('mapvote-d-info') != null){
-                    var infoText = document.getElementById('mapvote-d-info'); //größer fett und weiß
-                    infoText.style.color = "#fff";
-                    infoText.style.fontSize = "20px";
-                    infoText.style.fontWeight = "bold";
+                if(document.getElementById('mapvote')!=null && document.getElementsByClassName('mapvote-head-status').length != 0){
+                    var infoText = document.getElementById('mapvote-d-info');
+                    if(infoText != null){
+                        infoText.style.color = "#fff";
+                        infoText.style.fontSize = "20px";
+                        infoText.style.fontWeight = "bold";
+                    }
+                    var timer = document.getElementById('mapvote-d-timer');
+                    if(timer !=null){
+                        timer.style.color = "#fff";
+                    }
 
-                    document.getElementById('mapvote-d-timer').style.color = "#fff"; // weiß
+                    if(doOnce == 0){
+                        doOnce=1;
+                        document.getElementById('mapvote-d-team1').style.display = 'none';
+                        document.getElementById('mapvote-d-team2').style.display = 'none';
+                        moveAndShowVote();
+                    }
+                }
 
-                    Array.prototype.slice.call(document.getElementById('mapvote').getElementsByClassName('mapvote-head-team')).forEach((item) => {
-                        item.style.display = 'none';
-                    });
+                if(document.getElementById('mapvote')!=null && document.getElementsByClassName('mapvote-maps-done').length != 0){
                     clearInterval(intervalChangeHeader);
                 }
-            },100);
+            },50);
 
             var intervalAfterVote = setInterval(() => {
                 console.log("look for vote end");
@@ -120,14 +131,13 @@ position: absolute;
                 console.log("look for banned");
                 if(document.getElementById('mapvote')!=null && document.getElementById('mapvote').getElementsByClassName('banned').length != 0){
                     Array.prototype.slice.call(document.getElementById('mapvote').getElementsByClassName('banned')).forEach((item) => {
-                        item.style.opacity = "1";
-                        item.innerHTML = '<div class="line1"></div><div class="line2"></div>'
+                        item.innerHTML = '<div class="cross1"></div><div class="cross2"></div>'
                     });
                 }
                 if(document.getElementById('mapvote')!=null && document.getElementById('mapvote').getElementsByClassName('picked').length != 0){
                     Array.prototype.slice.call(document.getElementById('mapvote').getElementsByClassName('picked')).forEach((item) => {
                         item.style.opacity = "1";
-                        item.innerHTML = '<div class="strich1"></div><div class="strich2"></div>'
+                        item.innerHTML = '<div class="checkmark1"></div><div class="checkmark2"></div>'
                     });
                 }
                 if(document.getElementsByClassName('mapvote-maps-done').length != 0){
@@ -158,6 +168,7 @@ position: absolute;
                 item.style.width = "200px";
                 item.style.height = "200px";
                 item.style.border = "none";
+                item.style.opacity = "1";
                 item.innerHTML = "";
             }
         });
